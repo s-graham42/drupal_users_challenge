a283b31abb83c75576eac0e17c499d7f31e12442
Stephen Graham <sgraham42@gmail.com>
2021-07-08 14:58:57 -0700

    split up jquery tooltips for better alignment

--- themes/custom/challenge/js/scripts.js
+++ themes/custom/challenge/js/scripts.js
@@ -7,13 +7,25 @@
         $( ".move-down-tooltip" ).tooltip({
             show: null,
             position: {
-            my: "center top",
-            at: "center bottom"
+            my: "left+50 top",
+            at: "left+50 bottom",
             },
             open: function( event, ui ) {
             ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "medium" );
             }
         });
+
+        $( ".user-down-tooltip" ).tooltip({
+            show: null,
+            position: {
+            my: "center top",
+            at: "center bottom",
+            },
+            open: function( event, ui ) {
+            ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "medium" );
+            }
+        });
+
     } );
 
     $(function() {
