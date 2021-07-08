d618f11cd1cf55ff84ebd4ca3d0ff8e1428a89bd
Stephen Graham <sgraham42@gmail.com>
2021-07-08 00:35:08 -0700

    Jquery tooltip complete

--- themes/custom/challenge/js/scripts.js
+++ themes/custom/challenge/js/scripts.js
@@ -2,8 +2,41 @@
 
 console.log("hello from my scripts file!")
 
-$( document ).ready(function() {
-    console.log( "Jquery is ready!" );
+$( function() {
+    // JQuery UI Tooltip.
+    $( ".move-down-tooltip" ).tooltip({
+        show: null,
+        position: {
+        my: "center top",
+        at: "center bottom"
+        },
+        open: function( event, ui ) {
+        ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "medium" );
+        }
+    });
+} );
+
+$(function() {
+    console.log( "article function ready!" );
+    findArticles();
 });
+
+function findArticles() {
+    console.log('findArticles');
+    var $articles = $('article');
+    console.log('number of articles: ' + $articles.length);
+
+    for (const article in $articles) {
+    console.log(article);
+    // /console.log($articles.eq(x).children('.field-user-fullname').text());
+    // console.log($articles.eq(x).children('.field--name-field-first-name').length);/
+    // //console.log($('article').eq(x).children('.nodecontent').children('.field--name-field-first-name').children('.fielditem').text());
+    // //console.log($('article').eq(x).children('.nodecontent').children('.field--name-field-last-name').children('.fielditem').text());
+    // var first = $('article').eq(x).children('.nodecontent').children('.field--name-field-first-name').children('.fielditem').text();
+    // var last = $('article').eq(x).children('.nodecontent').children('.field--name-field-last-name').children('.fielditem').text();
+    // console.log(first+" "+last);
+    // $articles.eq(x).children('.field-user-fullname').html(first+" "+last);
+    }
+}
 
 }(jQuery));
